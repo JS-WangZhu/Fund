51650ab408281c6cf4acd311b3f8d70da98aaf64
wang zhu <361489555@qq.com>
2020-08-10 15:01:45 +0800

    commit

--- README.md
+++ README.md
@@ -3,10 +3,18 @@
 代码是Python3实现，爬取天天基金网站的相关基金估值与净值，喜欢玩基金的小伙伴一定不要错过
 
 # 使用步骤
-1. 安装相关依赖库 
-依赖库列表：beautifulsoup4, requests, prettytable
+1. 安装依赖 
+  Python3 依赖库列表：beautifulsoup4, requests, selenium, prettytable (pip安装即可)
+
+  Phantomjs 模拟浏览器访问 (官网下载即可，记得修改代码中的本地调用地址)
+
 2. 在目录下创建my.txt，文本格式：每行填写一个需要查询的基金号码,结尾不留空行,并以utf-8编码保存
+
 3. 运行fund.py
 
 # 项目运行
 ![项目运行截图](https://github.com/JS-WangZhu/Fund/blob/master/pic.png)
+
+# 更新
+
+1. 加入大盘指数查看功能
--- fund.py
+++ fund.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-# author: JS-WangZhu
-
-
 # 查询具体基金
 
 import os
@@ -11,6 +6,9 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import prettytable as pt
+from selenium import webdriver
+import warnings
+warnings.filterwarnings("ignore")
 
 # 获取所有基金信息
 def get_allinfo():
@@ -52,17 +50,17 @@ else:
 # 获取净值和估值 
 def get_value(url):
     headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.71 Safari/537.1 LBBROWSER'}
-    proxies={'http':'http://113.195.20.205:9999','http':'http://123.55.102.44:9999'}
+    #proxies={'http':'http://113.195.20.205:9999','http':'http://123.55.102.44:9999'}
     #res = requests.get(url,headers=headers,proxies=proxies)
-    res = requests.get(url,headers=headers)
-    soup = BeautifulSoup(res.content, 'html.parser',from_encoding="gb18030")
+    browser = webdriver.PhantomJS(executable_path='/Users/wangzhu/myFile/OpenPackages/phantomjs-2.1.1-macosx/bin/phantomjs')
+    browser.get(url)
+    res = browser.page_source
+    soup = BeautifulSoup(res, 'html.parser',from_encoding="gb18030")
     dataOfFund = soup.find_all('div', class_='dataOfFund')
     guzhi = dataOfFund[0].select('#gz_gszzl')[0].get_text()
-    gutime = dataOfFund[0].select('#gz_gztime')[0].get_text()[4:-1]
-
+    gutime = dataOfFund[0].select('#gz_gztime')[0].get_text()[5:]
     jing = dataOfFund[0].find_all('span',class_='ui-font-middle')[4]
     jingzhi = jing.get_text()
-
     jingzhi_t = soup.find_all('dl', class_='dataItem02')
     jingtime = re.findall('\</span>(.*?)\)',str(jingzhi_t[0]))[0][14:]
     return guzhi,gutime,jingzhi,jingtime
@@ -74,14 +72,34 @@ def getUrl(no):
             ind = index
     return fund_info[1][ind],fund_info[2][ind]
 
-        
+
+def getDapan():
+    url = 'http://quote.eastmoney.com/center/qqzs.html'
+    headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.71 Safari/537.1 LBBROWSER'}
+    browser = webdriver.PhantomJS(executable_path='/Users/wangzhu/myFile/OpenPackages/phantomjs-2.1.1-macosx/bin/phantomjs')
+    browser.get(url)
+    res = browser.page_source
+    soup = BeautifulSoup(res, 'lxml',from_encoding="gb18030")
+    dataTables = soup.find_all('tbody')
+#     print(dataTables)
+
+    jiage =  dataTables[0].find_all('td','mywidth2')
+    shangz_jg = jiage[0].get_text()
+    shangz_zd = jiage[1].get_text()
+    shenz_jg = jiage[2].get_text()
+    shenz_zd = jiage[3].get_text()
+    chuangyb_jg = jiage[6].get_text()
+    chuangyb_zd = jiage[7].get_text()
+    return shangz_jg,shangz_zd,shenz_jg,shenz_zd,chuangyb_jg,chuangyb_zd
+    
 
 if os.path.exists('./my.txt'):
     f = open('./my.txt','r')
+    shangz_jg,shangz_zd,shenz_jg,shenz_zd,chuangyb_jg,chuangyb_zd = getDapan()
     tb = pt.PrettyTable()
+    
     tb.field_names = ["基金代码", "基金名称", "估值", "估值更新", "净值", "净值更新"]
-    tb.padding_width = 1
-
+    
     content = f.readlines()
     for i in content:
         if str(i).endswith('\n'):
@@ -90,17 +108,15 @@ if os.path.exists('./my.txt'):
         try:
             guzhi,gutime,jingzhi,jingtime = get_value(tmpUrl)
         except:
-            continue
-        
+            continue    
         tb.add_row([i,tmpName,guzhi,gutime,jingzhi,jingtime])
-    tb.reversesort = True
+    
+    tb1 = pt.PrettyTable(['上证指数','上证涨跌','深证指数','深证涨跌','创业板指数','创业板涨跌'])
+    tb1.add_row([shangz_jg,shangz_zd,shenz_jg,shenz_zd,chuangyb_jg,chuangyb_zd])
+    print(tb1)
     print(tb)
     
 else:
     print('请在执行目录下创建my.txt,并按格式写入内容')
     print('格式：每行填写一个基金号码,结尾不留空行,以utf-8编码保存')
     print('创建成功后请重新执行此程序')
-
-
-
-
